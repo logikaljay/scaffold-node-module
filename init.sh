46775409bf8383d6faf02f9f4b6eb0d8d8561092
Jay Baker <logikal@gmail.com>
2015-12-04 12:56:06 +1300

    feat(Init): added an init script for ease of download

---
+++ init.sh
@@ -0,0 +1,24 @@
+echo "What do you want to call this module? (some-node-module)"
+read DEST
+
+VERSION=1.0.0
+URL="https://github.com/logikaljay/scaffold-node-module/archive/v${VERSION}.zip"
+SRC="scaffold-node-module-${VERSION}"
+
+echo "Downloading ${URL}"
+curl -L -s ${URL} \
+     -o tmp-module.zip \
+     && echo "Extracting." \
+     && unzip -qq tmp-module.zip \
+     && echo "Cleaning up." \
+     && mv ${SRC} ${DEST} \
+     && rm -f "tmp-module.zip" \
+     && echo "Running 'npm install'" \
+     && cd ${DEST} \
+     && sed -i '' "s/https:\/\/github.com\/logikaljay\/scaffold-node-module.git//g" package.json \
+     && sed -i '' "s/scaffold-node-module/${DEST}/g" package.json \
+     && sed -i '' "s/Node module for scaffolding//g" package.json \
+     && sed -i '' "s/Jay Baker//g" package.json \
+     && echo "Done!" \
+     && echo "Your module is now available at: ./${DEST}"
+     
