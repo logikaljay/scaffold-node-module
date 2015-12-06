1f3d1af61fb4fd60572097bebf7964b6153aef7a
Jay Baker <logikal@gmail.com>
2015-12-07 08:33:26 +1300

    feat(Typings): Added tsd.json for node typings

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 echo "What do you want to call this module? (some-node-module)"
 read DEST
 
-VERSION=1.1.1
+VERSION=1.2.1
 URL="https://github.com/logikaljay/scaffold-node-module/archive/v${VERSION}.zip"
 SRC="scaffold-node-module-${VERSION}"
 
